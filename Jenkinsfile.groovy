f721837538cb668c7c05f09f0316bcbd7b501f4f
Clinton-Devs <kingstonwhyte87@gmail.com>
2025-11-24 16:08:54 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,8 @@ pipeline {
     }
     stage('Build') {
       steps {
-        def appImage = "dwhyte40/vhennus_server:${env.BUILD_NUMBER}"
+        //def appImage = "dwhyte40/vhennus_server:${env.BUILD_NUMBER}"
+         sh 'cargo build --release' 
       }
     }
     stage('Test') {
